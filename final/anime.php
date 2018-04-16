93983e86255b475a3743539fbe7f8bd9dea0594a
GlairMark <35548823+GlairMark@users.noreply.github.com>
2018-04-16 12:04:40 -0400

    Add files via upload

---
+++ final/anime.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+
+<title>Final Project</title>
+<meta name="description" content="Recommended Anime List">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="anime.css" rel="stylesheet">
+</head>
+<link rel="icon" href="favicon.jpg" type="image/jpg" sizes="16x16">
+<body>
+<div id="wrapper">
+<nav>
+<img class="imgprofile" src="MyImage.png" alt="GlairBronola">
+  <ul>
+  <li><a href="index.html">Home</a></li>
+  <li><a href="action.html">Action</a></li>
+  <li><a href="adventure.html">Adventure</a></li>
+  <li><a href="comedy.html">Comedy</a></li>
+  <li><a href="fantasy.html">Fantasy</a></li>
+  <li><a href="shop.html">Shop</a></li>
+  <li><a href="faq.html">FAQ</a></li>
+  <li><a href="contact.html">Contact Me</a></li>
+  <li><a href="sitemap.html">SiteMap</a></li>
+  </ul>
+</nav>
+<main>
+<h1> I have received your Email. Wait for a couple of days to recieve a reply. Thank you for your time. <h1>
+<form action="#">
+<input type="button" value = "Back" onclick="javascript:history.go(-1)" />
+</main>
+</form>
+</div>
+</div>
+</body>
+</html>
